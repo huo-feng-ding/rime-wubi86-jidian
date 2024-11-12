a8113a47ff60285b0974b02e5aa7b4a3e38499c8
xinsheng <wangxinsh55@126.com>
2024-11-12 18:42:47 +0800

    临时英文lua脚本优化

--- lua/append_original_filter.lua
+++ lua/append_original_filter.lua
@@ -16,37 +16,33 @@ local function append_original_filter(input, env)
         local segInputLen = string.len(segInput)
         if schema.schema_id == "easy_en" then
             yield(Candidate("string", seg.start, seg._end, segInput, ""))
-        elseif segInput:sub(1, 1) == ";"  then
-            -- 分号是引导的，输入字符大于一个才有效
-            if segInputLen > 1 then 
-                -- 这里和正则字符集和 default.custom.yaml recognizer/patterns/english 配置有关联
-                local word = string.match(segInput, ".*[-._+'a-zA-Z]$")
-                -- log.info('s', segInput, word==nil, segInputLen, string.sub(segInput,segInputLen, segInputLen), string.sub(segInput,segInputLen, segInputLen)==";")
-                -- 如果是英语单词，则在候选栏里显示
-                if word ~= nil then 
-                    -- log.info('t', segInput)
-                    yield(Candidate("word", seg.start, seg._end, string.sub(segInput, 2), ""))
-                elseif string.sub(segInput,segInputLen, segInputLen) == ";" then
-                    -- 如果是以分号结尾的则上屏
-                    -- log.info('u', segInput)
+        elseif segInput:match("^[;A-Z][-.;_+'a-zA-Z]*$") then 
+            -- 上边的正则表达式和 default.custom.yaml recognizer/patterns/english 配置有关联
+            -- log.info('inputInfo:', segInput, segInputLen, string.sub(segInput,segInputLen, segInputLen), string.sub(segInput,segInputLen, segInputLen)==";")
+            
+            -- 以分号开头引导临时英
+            if segInput:sub(1, 1) == ";" and segInputLen > 1 then
+                -- 如果是以分号结尾的则上屏, 其它字符结尾的会自动顶屏
+                if string.sub(segInput,segInputLen, segInputLen) == ";" then
                     envengine:commit_text(string.sub(segInput, 2))
                     envcontext:clear()
                     return
+                else 
+                    yield(Candidate("word", seg.start, seg._end, string.sub(segInput, 2), ""))
                 end
-            end
-        else
-            local word = string.match(segInput, "^[A-Z][-._+'a-zA-Z]*$")
-            -- 如果是以大写字母开关的,则在候选栏里显示
-            if word ~= nil then 
-                yield(Candidate("word", seg.start, seg._end, segInput, ""))
-            elseif string.sub(segInput,segInputLen, segInputLen) == ";" then
-                -- 如果是以分号结尾的则上屏
-                envengine:commit_text(segInput, 2)
-                envcontext:clear()
-                return
+            elseif segInput:match("^[A-Z]") then
+                -- 以大写开头引导临时英文, 以分号结尾则上屏
+                if string.sub(segInput,segInputLen, segInputLen) == ";" then
+                    envengine:commit_text(segInput, 2)
+                    envcontext:clear()
+                    return
+                else
+                    yield(Candidate("word", seg.start, seg._end, segInput, ""))
+                end
             end
         end
     end
+    
     for cand in input:iter() do
         yield(cand)
     end
